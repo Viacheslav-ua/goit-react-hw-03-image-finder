f448eee008e23162328150fd5d465b0cf1c6a71b
Viacheslav-ua <75425740+Viacheslav-ua@users.noreply.github.com>
2021-10-13 22:48:39 +0300

    add search api

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { type } from "os";
 import React, { Component } from "react";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import "./App.css";
 import ImageGallery from "./components/ImageGallery";
 import ImageGalleryItem from "./components/ImageGalleryItem";
@@ -13,56 +15,86 @@ type Picture = {
 
 interface StateTypes {
   pictures: Picture[];
+  keyword: string;
+  page: number;
   loading: boolean;
 }
 class App extends Component {
   state: StateTypes = {
     pictures: [],
+    keyword: "",
+    page: 1,
     loading: false,
   };
 
   componentDidMount() {
-    // .then(console.log);
+    console.log('DidMount')
   }
 
-  responseAPI = () => {
+  componentDidUpdate(prevProps: any, prevState: StateTypes) {
+    if(prevState.keyword !== this.state.keyword ||
+      prevState.page !== this.state.page){
+      this.responseAPI(this.state.keyword, this.state.page)
+    }
+    
+  }
+
+  responseAPI = (keyword: string, page: number) => {
     const BASE_URL = "https://pixabay.com/api/";
     const API_KEY = "22997657-91d4620e666f378f8f41767fa";
     this.setState({ loading: true });
     fetch(
-      `${BASE_URL}?q=s&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+      `${BASE_URL}?q=${keyword}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
     )
       .then((res) => res.json())
-      // .then(console.log);
       .then((res) => this.getPictures(res))
       .finally(() => this.setState({ loading: false }));
   };
 
   getPictures = (obj: any) => {
-    const pictures = obj.hits.map((item: Picture) => {
+    const arrPictures = obj.hits.map((item: Picture) => {
       return {
         id: item.id,
         webformatURL: item.webformatURL,
         largeImageURL: item.largeImageURL,
       };
     });
-    this.setState({ pictures: pictures });
+    
+    this.setState((prevState: StateTypes) => ({pictures: [...prevState.pictures, ...arrPictures]}));
+    
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: 'smooth',
+    });
   };
+
+  setKeyword = (str: string) => {
+    this.setState({
+      keyword: str,
+      page: 1,
+      pictures: [], 
+    });
+  }
+
+  onLoadMore = () => {
+    this.setState((prevState: StateTypes) => ({  page: prevState.page + 1 }));
+  }
 
   render() {
     return (
-      <>
-        <Searchbar />
-        <button type="button" onClick={this.responseAPI}>
-          GETAPI
-        </button>
+      <div className="App">
+        <Searchbar onSubmit={this.setKeyword}/>
+        
         <ImageGallery>
           {this.state.loading && <p>Loading...</p>}
           {this.state.pictures.map((item: Picture) => (
             <ImageGalleryItem key={item.id} webformatURL={item.webformatURL} />
           ))}
+          
         </ImageGallery>
-      </>
+        <button type="button" onClick={this.onLoadMore} className="Button">Load more</button>
+        <ToastContainer autoClose={3000}/>
+      </div>
     );
   }
 }
--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,12 +1,34 @@
 import React, { Component } from "react";
 import { ImSearch } from "react-icons/im";
 import S from "./Searchbar.module.css";
+import { toast } from 'react-toastify';
 
-export default class Searchbar extends Component {
+interface propsType {
+  onSubmit: any;
+}
+
+export default class Searchbar extends Component<propsType> {
+  state = {
+    keyword: "",
+  }
+
+  handleInputChang = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ [e.currentTarget.name]: e.currentTarget.value });
+  }
+
+  handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (this.state.keyword.trim() === "") {
+      toast.error("Enter a word");
+      return
+    }
+    this.props.onSubmit(this.state.keyword)
+  }
+
   render() {
     return (
       <header className={S.Searchbar}>
-        <form className={S.SearchForm}>
+        <form className={S.SearchForm} onSubmit={this.handleSubmit}>
           <button type="submit" className={S.Button}>
             <ImSearch />
             <span className={S.ButtonLabel}>Search</span>
@@ -14,7 +36,10 @@ export default class Searchbar extends Component {
 
           <input
             className={S.Input}
+            name="keyword"
+            value={this.state.keyword}
             type="text"
+            onChange={this.handleInputChang}
             autoComplete="off"
             autoFocus
             placeholder="Search images and photos"
